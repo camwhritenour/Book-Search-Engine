a993f578b44fde6de728c25b840d164c822d9ee2
unknown <whritenourc@outlook.com>
2024-04-22 16:51:36 -0500

    added resolvers typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,65 @@
+const { Book, User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+        me: async () => {
+            return User.findOne({
+                $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+            });
+        },
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+
+        saveBook: async (parent, { user, body }) => {
+            try {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: user._id },
+                    { $addToSet: { savedBooks: body } },
+                    { new: true, runValidators: true }
+                );
+                return { updatedUser };
+            } catch (err) {
+                console.log(err);
+                return res.status(400).json(err);
+            }
+        },
+
+        removeBook: async (parent, { user, params }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+            );
+            if (!updatedUser) {
+                return res.status(404).json({ message: "Couldn't find user with this id!" });
+            }
+            return { updatedUser };
+        }
+    },
+};
+
+module.exports = resolvers;
